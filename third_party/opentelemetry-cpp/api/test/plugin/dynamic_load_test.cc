96b4933f54de925b84ef1bfd93106dd411968e3a
brinkqiang <brink.qiang@gmail.com>
2024-12-10 18:08:16 +0800

    Reset repository

---
+++ third_party/opentelemetry-cpp/api/test/plugin/dynamic_load_test.cc
@@ -0,0 +1,15 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+#include <gtest/gtest.h>
+#include <string>
+
+#include "opentelemetry/plugin/dynamic_load.h"
+
+TEST(LoadFactoryTest, FailureTest)
+{
+  std::string error_message;
+  auto factory = opentelemetry::plugin::LoadFactory("no-such-plugin", error_message);
+  EXPECT_EQ(factory, nullptr);
+  EXPECT_FALSE(error_message.empty());
+}
